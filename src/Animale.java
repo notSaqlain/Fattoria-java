bd4e0e3636aec28c3b8280096ec03ace352d3517
notSaqlain <iamnotsaqlain@gmail.com>
2025-01-24 08:48:58 +0100

    feat: Add Animal and Dog classes with basic methods
    This commit adds the Animal and Dog classes to the codebase. The Animal class provides basic methods for the breed, sound, species, and movement of an animal. The Dog class extends the Animal class and provides specific implementations for a dog, including the breed "German Shepherd," the sound "Bau," the species "Canis lupus familiaris," and the movement "Run."

---
+++ src/Animale.java
@@ -0,0 +1,17 @@
+public class Animale {
+    public String razza() {
+        return "";
+    }
+
+    public String verso() {
+        return "";
+    }
+
+    public String specie() {
+        return "";
+    }
+
+    public String moviti() {
+        return "";
+    }
+}
---
+++ src/Cane.java
@@ -0,0 +1,18 @@
+public class Cane extends Animale {
+    public String razza() {
+        return "Pastore tedesco";
+    }
+
+    public String verso() {
+        return "Bau";
+    }
+
+    public String specie() {
+        return "Canis lupus familiaris";
+    }
+
+    public String moviti() {
+        return "Corri";
+    }
+    
+}
