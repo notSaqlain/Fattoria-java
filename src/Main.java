81a53ff711850dd28dcdc48a51163485cd43cc11
notSaqlain <iamnotsaqlain@gmail.com>
2025-01-24 09:08:07 +0100

    Refactor Main.java to remove unnecessary code and improve readability

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,8 @@ public class Main {
     public static void main(String[] args) throws Exception {
         
         Cane Barboncino = new Cane("Barboncino", "Bau", "Corre");
-        Animale baubau = new Animale("Canis", "Pastore Tedesco", "bau", "corre");
+        //Animale baubau = new Animale("Canis", "Pastore Tedesco", "bau", "corre");
 
         System.out.println(Barboncino);
-        System.out.println();
-        System.out.println(baubau);
     }
 }
